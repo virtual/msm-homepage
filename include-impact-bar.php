087c4cb0f1fdfc977051fa7e62288f4eac7eb1bc
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-05-02 13:52:30 -0600

    add styles to impact bar

--- include-impact-bar.php
+++ include-impact-bar.php
@@ -1,19 +1,21 @@
-<div class="background-primary background-inverse background-custom wrapper">
-    <div class="column-label-event row" data-equalizer-watch="">
-        <section class="impact-bar">
-            <div class="large-4 medium-3 small-12 columns fancy-heading">
+<div class="background-primary background-inverse background-custom wrapper impact-bar">
+    <div class="column-label-event row">
+        <section class="small-12">
+           <div class="row" data-equalizer-watch="">
+            <div class="large-3 medium-4 small-12 columns fancy-heading">
                     <p><em>{ your gift has impact }</em></p>
 
             </div>
-            <div class="large-6 medium-7 small-12 columns">
+            <div class="large-7 medium-6 small-12 columns">
                 <p>Provide critical support and bridge the gap between tuition and the true costs of a health professions education. Make your quick and easy gift today.</p>
             </div>
-            <div class="large-2 medium-2 small-2 columns">
+            <div class="large-2 medium-2 small-12 columns">
                 <ul class="text-center button-list blue">
 
             <li><a href="http://www.msm.edu/news-center/">Invest&nbsp;Today</a></li>
 
         </ul>
+           </div>
             </div>
         </section>
     </div>
