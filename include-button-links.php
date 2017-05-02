c7cc0a90068dc9f6985d62135d443959d5a298aa
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-05-02 16:15:30 -0600

    add event styling

--- include-button-links.php
+++ include-button-links.php
@@ -3,22 +3,24 @@
         <section class="button-links text-center">
             <ul class="three-columns">
                 <li class="small-12 medium-4 columns">
-                    <span class="fa fa-envira fa-5x"><span class="sr-only">Leaf</span></span>
+                    <p><a href="http://www.msm.edu/StrategicPlan/index.php"><span class="fa fa-envira fa-5x"><span class="sr-only">Leaf</span></span>
 
-                    <p><a href="#"><strong>Excellence Through Health Equity</strong><br/>
+                    <strong>Excellence Through Health Equity</strong> 
                        MSM Strategic Plan 2015&ndash;2020</a></p>
                 </li>
                 <li class=" small-12 medium-4 columns">
-                    <span class="fa fa-heartbeat fa-5x"><span class="sr-only">heartbeat</span></span>
+                    <p><a href="https://www.youtube.com/playlist?list=PLTpetkN815QxIB1ab38aDp11x6mP_Dej3"><span class="fa fa-heartbeat fa-5x"><span class="sr-only">heartbeat</span></span>
 
-                    <p><a href="#"><strong>Health Currents</strong><br/>
+                    <strong>Health Currents</strong> 
                        Resources for Healthy Living</a></p>
                 </li>
                 <li class=" small-12 medium-4 columns">
-                    <span class="fa fa-gift fa-5x"><span class="sr-only">gift</span></span>
+                    <p>
+                        <a href="http://giving.msm.edu/">
+                            <span class="fa fa-gift fa-5x"><span class="sr-only">gift</span></span>
 
-                    <p><a href="#"><strong>Donate</strong><br/>
-                       Make a Difference</a></p>
+                            <strong>Donate</strong> Make a Difference</a>
+                    </p>
                 </li>
             </ul>
         </section>
--- include-events.php
+++ include-events.php
@@ -2,45 +2,80 @@
     <div class="column-label-event row" data-equalizer-watch="">
         <section class="msm-events">
             <ul>
-                <li class=" small-6 medium-3 columns">
-                    <span class="fa-stack fa-3x">
-    <span class="fa fa-calendar-o fa-stack-2x"><span class="sr-only">Date</span></span>
-                    <strong class="fa-stack-1x calendar-text"><span class="calendar-text-month">Mar</span><span class="calendar-text-day">27</span></strong>
-                    </span>
-                    <p><a href="#">An Introduction to Bariatric Surgery</a>
-                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span></p>
+                <li class="small-6 medium-3 columns">
+                    <a href="#">
+                        <span class="fa-stack fa-3x">
+                            <span class="fa fa-calendar-o fa-stack-2x">
+                                <span class="sr-only">date</span>
+                            </span>
+                            <span class="fa-stack-1x calendar-text">
+                                <span class="calendar-text-month">May</span>
+                                <span class="calendar-text-day">2</span>
+                            </span>
+                        </span>
+                        
+                        <strong>David Satcher, MD, PhD, Health Equity Research Seminar Series: Curbing the Opioid Epidemic - The Healthcare Professional's Role</strong>
+                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span>
+                    </a>
                 </li>
-                <li class=" small-6 medium-3 columns">
-                    <span class="fa-stack fa-3x">
-    <span class="fa fa-calendar-o fa-stack-2x"><span class="sr-only">Date</span></span>
-                    <strong class="fa-stack-1x calendar-text"><span class="calendar-text-month">Mar</span><span class="calendar-text-day">27</span></strong>
-                    </span>
-                    <p><a href="#">An Introduction to Bariatric Surgery</a>
-                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span></p>
+                <li class="small-6 medium-3 columns">
+                    <a href="#">
+                        <span class="fa-stack fa-3x">
+                            <span class="fa fa-calendar-o fa-stack-2x">
+                                <span class="sr-only">date</span>
+                            </span>
+                            <span class="fa-stack-1x calendar-text">
+                                <span class="calendar-text-month">May</span>
+                                <span class="calendar-text-day">2</span>
+                            </span>
+                        </span>
+                        
+                        <strong>Grand Rounds: High Reliability Organization and Grady</strong>
+                        <span class="event-date">Wednesday, May 3, 2017 12:00 PM - 1:00 PM</span>
+                    </a>
                 </li>
-                <li class=" small-6 medium-3 columns">
-                    <span class="fa-stack fa-3x">
-    <span class="fa fa-calendar-o fa-stack-2x"><span class="sr-only">Date</span></span>
-                    <strong class="fa-stack-1x calendar-text"><span class="calendar-text-month">Mar</span><span class="calendar-text-day">27</span></strong>
-                    </span>
-                    <p><a href="#">An Introduction to Bariatric Surgery</a>
-                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span></p>
+                <li class="small-6 medium-3 columns">
+                    <a href="#">
+                        <span class="fa-stack fa-3x">
+                            <span class="fa fa-calendar-o fa-stack-2x">
+                                <span class="sr-only">date</span>
+                            </span>
+                            <span class="fa-stack-1x calendar-text">
+                                <span class="calendar-text-month">May</span>
+                                <span class="calendar-text-day">2</span>
+                            </span>
+                        </span>
+                        
+                        <strong>David Satcher, MD, PhD, Health Equity Research Seminar Series: Curbing the Opioid Epidemic - The Healthcare Professional's Role</strong>
+                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span>
+                    </a>
                 </li>
-                <li class=" small-6 medium-3 columns">
-                    <span class="fa-stack fa-3x">
-    <span class="fa fa-calendar-o fa-stack-2x"><span class="sr-only">Date</span></span>
-                    <strong class="fa-stack-1x calendar-text"><span class="calendar-text-month">Mar</span><span class="calendar-text-day">27</span></strong>
-                    </span>
-                    <p><a href="#">An Introduction to Bariatric Surgery</a>
-                        <span class="event-date">Wednesday, April 19, 2017 5:30 PM - 7:30 PM</span></p>
+                <li class="small-6 medium-3 columns">
+                    <a href="#">
+                        <span class="fa-stack fa-3x">
+                            <span class="fa fa-calendar-o fa-stack-2x">
+                                <span class="sr-only">date</span>
+                            </span>
+                            <span class="fa-stack-1x calendar-text">
+                                <span class="calendar-text-month">May</span>
+                                <span class="calendar-text-day">3</span>
+                            </span>
+                        </span>
+                        
+                        <strong>MSM Town Hall Meeting</strong>
+                        <span class="event-date">Wed, May 3, 2017 4:00 PM - 5:30 PM</span>
+                    </a>
                 </li>
 
             </ul>
 
         </section>
+
+    </div>
+    <div class="row">
         <ul class="text-center button-list blue">
 
-            <li><a href="http://www.msm.edu/news-center/">View Events</a></li>
+            <li><a href="http://calendar.msm.edu/">View Events</a></li>
 
         </ul>
     </div>
